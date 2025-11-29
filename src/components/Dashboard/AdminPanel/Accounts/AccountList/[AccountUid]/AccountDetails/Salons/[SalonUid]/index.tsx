244f6dbc120d962e13a84c63066b981bc43491b8
mahbub045 <mrahman172014@bscse.uiu.ac.bd>
2025-11-29 22:33:17 +0600

    feat(admin-panel): add SalonDetails and SalonDetailsContainer components for detailed salon view

--- src/components/Dashboard/AdminPanel/Accounts/AccountList/[AccountUid]/AccountDetails/Salons/[SalonUid]/index.tsx
+++ src/components/Dashboard/AdminPanel/Accounts/AccountList/[AccountUid]/AccountDetails/Salons/[SalonUid]/index.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Breadcrumbs from "@/components/Dashboard/CommonComponents/Breadcrumbs";
+import { useParams } from "next/navigation";
+import SalonDetails from "./SalonDetails/SalonDetails";
+
 const SalonDetailsContainer: React.FC = () => {
-  return <div>
-    Salon Details Container
-  </div>;
+  const { accountuid, salonuid } = useParams();
+  return (
+    <div>
+      <Breadcrumbs
+        items={[
+          { label: "Home", href: "/dashboard/admin-panel" },
+          {
+            label: "Accounts",
+            href: "/dashboard/admin-panel/accounts",
+          },
+          {
+            label: " Account Details",
+            href: `/dashboard/admin-panel/accounts/${accountuid}`,
+          },
+          {
+            label: `Salon Details`,
+            href: `/dashboard/admin-panel/accounts/${accountuid}/salons/${salonuid}`,
+          },
+        ]}
+      />
+      <SalonDetails />
+    </div>
+  );
 };
 
 export default SalonDetailsContainer;
