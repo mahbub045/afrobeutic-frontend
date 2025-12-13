bf07f45c7fa8eb0049805c70bae26e4080dca2a3
mahbub045 <mrahman172014@bscse.uiu.ac.bd>
2025-12-13 20:01:28 +0600

    Remove commented domains entry in next.config.ts for clarity

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    // domains: ["api.afrobeutic.com"],
     remotePatterns: [
       {
         protocol: "https",
