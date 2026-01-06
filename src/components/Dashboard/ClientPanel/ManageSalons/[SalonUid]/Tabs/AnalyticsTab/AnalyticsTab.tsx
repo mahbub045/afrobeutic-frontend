3b21bf23e5de99c2c74bb8e1323c0ebc7d56f84e
mahbub045 <mrahman172014@bscse.uiu.ac.bd>
2026-01-06 22:36:37 +0600

    feat: add ProductRevenue component to AnalyticsTab for displaying product revenue data

--- src/components/Dashboard/ClientPanel/ManageSalons/[SalonUid]/Tabs/AnalyticsTab/AnalyticsTab.tsx
+++ src/components/Dashboard/ClientPanel/ManageSalons/[SalonUid]/Tabs/AnalyticsTab/AnalyticsTab.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ProductRevenue from "./ProductRevenue/ProductRevenue";
 import Revenue from "./Revenue/Revenue";
 import ServiceRevenue from "./ServiceRevenue/ServiceRevenue";
 
@@ -8,8 +9,9 @@ const AnalyticsTab: React.FC = () => {
       <div>
         <Revenue />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <ServiceRevenue />
+        <ProductRevenue />
       </div>
       <div></div>
       <div></div>
