9c85543462abe46ab103e93488c71623a57469d1
mashbub045 <mashbubrmn45@gmail.com>
2026-02-07 11:01:05 +0600

    refactor: remove Meta OAuth components and related SDK integration

--- src/Types/global.d.ts
+++ src/Types/global.d.ts
@@ -13,23 +13,3 @@ declare module "*.module.sass" {
   const classes: { [key: string]: string };
   export default classes;
 }
-
-declare global {
-  interface Window {
-    fbAsyncInit?: () => void;
-    FB?: {
-      init: (params: {
-        appId: string;
-        cookie?: boolean;
-        xfbml?: boolean;
-        version: string;
-      }) => void;
-      login: (
-        callback: (response: { authResponse?: { code?: string } }) => void,
-        options?: Record<string, unknown>,
-      ) => void;
-    };
-  }
-}
-
-export {};
