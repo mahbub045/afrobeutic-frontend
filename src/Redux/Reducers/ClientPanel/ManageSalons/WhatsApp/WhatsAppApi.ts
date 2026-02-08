02dae7b1040b03a13a200ca489ad99fdaca81fa8
mashbub045 <mashbubrmn45@gmail.com>
2026-02-08 20:22:06 +0600

    feat: add WhatsApp onboarding data query and update component to utilize new API

--- src/Redux/Reducers/ClientPanel/ManageSalons/WhatsApp/WhatsAppApi.ts
+++ src/Redux/Reducers/ClientPanel/ManageSalons/WhatsApp/WhatsAppApi.ts
@@ -2,6 +2,13 @@ import { baseApi } from "@/Redux/Api/BaseApi";
 
 export const WhatsAppApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+    getWhatsAppOnboardData: build.query({
+      query: (salonUid) => ({
+        url: `/salons/${salonUid}/whatsapp`,
+        method: "GET",
+      }),
+      providesTags: ["WhatsAppOnBoardData"],
+    }),
     whatsAppOnboard: build.mutation({
       query: ({ salonUid, ...wabaInfo }) => ({
         url: `/salons/${salonUid}/whatsapp`,
@@ -13,4 +20,5 @@ export const WhatsAppApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useWhatsAppOnboardMutation } = WhatsAppApi;
+export const { useGetWhatsAppOnboardDataQuery, useWhatsAppOnboardMutation } =
+  WhatsAppApi;
