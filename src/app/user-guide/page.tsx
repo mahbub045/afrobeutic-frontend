3b69a7696229675dc035ced6664b2b170ed5ee4b
mahbub045 <mahbubrmn45@gmail.com>
2026-02-24 18:42:44 +0600

    feat: enhance User Guide layout with NavBar and construction message; remove unused address input in EditBasicInfoDialog

--- src/app/user-guide/page.tsx
+++ src/app/user-guide/page.tsx
@@ -1,3 +1,13 @@
+import NavBar from "@/components/Layout/NavBar/NavBar";
+
 export default function UserGuide() {
-  return <div>UserGuide</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="text-danger flex h-screen flex-col items-center justify-center gap-1">
+        <h1>User Guide</h1>
+        <h4>This page is under construction.</h4>
+      </div>
+    </>
+  );
 }
