a6d5f9f28bbb7f6059f6a87d12dc460f7487d8b5
mahbub045 <mahbubrmn45@gmail.com>
2026-02-25 18:44:04 +0600

    refactor: initialize state variables for WhatsApp dialog management

--- src/components/Dashboard/ClientPanel/ManageSalons/[SalonUid]/Tabs/SettingsTab/WhatsApp/WhatsApp.tsx
+++ src/components/Dashboard/ClientPanel/ManageSalons/[SalonUid]/Tabs/SettingsTab/WhatsApp/WhatsApp.tsx
@@ -15,6 +15,11 @@ const WhatsApp: React.FC = () => {
   const { salonuid } = useParams();
   const [connectWhatsAppDialogOpen, setConnectWhatsAppDialogOpen] =
     useState(false);
+  const [deleteWhatsAppDialogOpen, setDeleteWhatsAppDialogOpen] =
+    useState(false);
+  const [selectedWhatsAppData, setSelectedWhatsAppData] = useState<
+    WhatsAppOnboardData | undefined
+  >(undefined);
 
   const handleConnectWhatsApp = () => {
     setConnectWhatsAppDialogOpen(true);
