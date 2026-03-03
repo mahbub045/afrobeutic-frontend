b117ee19e88b4f116a7c631ee8a55356b19274da
mahbub045 <mahbubrmn45@gmail.com>
2026-03-03 12:11:21 +0600

    feat: comment out broadcasting details in PlanLimitsCard and PricingPlanList components

--- src/components/Dashboard/ClientPanel/Accounts/PricingPlans/PricingPlanList/PricingPlanList.tsx
+++ src/components/Dashboard/ClientPanel/Accounts/PricingPlans/PricingPlanList/PricingPlanList.tsx
@@ -95,12 +95,12 @@ const PricingPlanList: React.FC = () => {
                       <strong>Chatbot Messages Limit -&gt;</strong>{" "}
                       {plan.whatsapp_messages_per_chatbot}
                     </li>
-                    <li>
+                    {/* <li>
                       <strong>Broadcasting -&gt;</strong>{" "}
                       {plan.has_broadcasting
                         ? `Yes (limit ${plan.broadcasting_message_limit})`
                         : "No"}
-                    </li>
+                    </li> */}
                   </ul>
                 </CardContent>
                 {plan.price === "0.00" ? null : (
